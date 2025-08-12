86ad4d618cf2f6581e393a0554bdf43687dbc00c
Juanes-xd <juanestebanortizbejarano@gmail.com>
2025-08-12 17:03:54 -0500

    Add task management functionality with CORS support
    - Implemented Task model with title, description, and done fields.
    - Created TaskSerializer for the Task model.
    - Added TaskViewSet for CRUD operations on tasks.
    - Configured URLs for task API endpoints.
    - Registered Task model in Django admin.
    - Enabled CORS middleware and specified allowed origins.

---
+++ tasks/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework import routers
+from tasks import views
+
+
+router = routers.DefaultRouter()
+router.register(r'tasks', views.TaskViewSet, 'tasks')
+
+
+urlpatterns = [
+    path('api/v1/', include(router.urls)),
+]
+
+
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
+from .models import Task
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from tasks.serializer import TaskSerializer
 # Create your views here.
+
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
